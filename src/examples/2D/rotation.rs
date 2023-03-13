2e6f3365149d3cdcae77d5bf330f17a5b130de8d
Dumpinground <e60533658@icloud.com>
2023-03-13 20:42:01 +0800

    update to bevy 0.10.0

--- src/examples/2D/rotation.rs
+++ src/examples/2D/rotation.rs
@@ -1,13 +1,22 @@
-use bevy::asset::AssetServer;
-use bevy::DefaultPlugins;
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+};
+use bevy::math::Vec3Swizzles;
 
+const TIME_STEP: f32 = 1.0 / 165.0; // 显示器刷新
 const BOUNDS: Vec2 = Vec2::new(1200.0, 640.0);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_systems(
+            (
+                player_movement_system,
+                snap_to_player_system,
+                rotate_to_player_system,
+            )
+        )
         .add_system(bevy::window::close_on_esc)
         .run();
 }
@@ -41,7 +50,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         SpriteBundle {
             texture: ship_handle,
             ..default()
-        }, 
+        },
         Player {
             movement_speed: 500.0,
             rotation_speed: f32::to_radians(360.0),
@@ -92,4 +101,43 @@ fn player_movement_system(
     mut query: Query<(&Player, &mut Transform)>,
 ) {
     let (ship, mut transform) = query.single_mut();
+
+    let mut rotation_factor = 0.0;
+    let mut movement_factor = 0.0;
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        rotation_factor += 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::Right) {
+        rotation_factor -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::Up) {
+        movement_factor += 1.0;
+    }
+
+    transform.rotate_z(rotation_factor * ship.rotation_speed * TIME_STEP);
+
+    let movement_direction = transform.rotation * Vec3::Y;
+    let movement_distance = movement_factor * ship.movement_speed * TIME_STEP;
+    let translation_delta = movement_direction * movement_distance;
+    transform.translation += translation_delta;
+
+    let extents = Vec3::from((BOUNDS / 2.0, 0.0));
+    transform.translation = transform.translation.min(extents).max(-extents);
 }
+
+fn snap_to_player_system(
+    mut query: Query<&mut Transform, (With<SnapToPlayer>, Without<Player>)>,
+    player_query: Query<&Transform, With<Player>>,
+) {
+    let player_transform = player_query.single();
+    let player_translation = player_transform.translation.xy();
+
+    for mut enemy_transform in &mut query {
+        let to_player = (player_translation - enemy_transform.translation.xy()).normalize();
+    }
+}
+
+fn rotate_to_player_system() {}
