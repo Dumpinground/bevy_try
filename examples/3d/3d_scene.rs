c661d98a302be42dd5e62364e3cd67a124c01baa
Ad <DeadLimit@lkr5.onmicrosoft.com>
2023-03-15 16:47:14 +0800

    mirror

--- examples/3d/3d_scene.rs
+++ examples/3d/3d_scene.rs
@@ -25,4 +25,19 @@ fn setup(
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
     });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
