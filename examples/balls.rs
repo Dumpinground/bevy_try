8acc5175aa8f1e46a34b1ff5cbff179f046f5f89
Ad <DeadLimit@lkr5.onmicrosoft.com>
2023-03-23 15:11:40 +0800

    update

---
+++ examples/balls.rs
@@ -0,0 +1,7 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new().run();
+}
+
+fn setup(mut commands: Commands) {}
---
+++ examples/custom_scene.rs
@@ -0,0 +1,109 @@
+use bevy::{input::mouse::MouseMotion, prelude::*};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cubes
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(2.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        CameraController::default(),
+    ));
+}
+
+#[derive(Component)]
+struct CameraController {
+    pub enabled: bool,
+
+    pub key_up: KeyCode,
+    pub key_down: KeyCode,
+
+    pub walk_speed: f32,
+    pub run_speed: f32,
+}
+
+impl Default for CameraController {
+    fn default() -> Self {
+        Self {
+            enabled: true,
+
+            key_up: KeyCode::Up,
+            key_down: KeyCode::Down,
+
+            walk_speed: 10.0,
+            run_speed: 30.0,
+        }
+    }
+}
+
+fn camera_controller(
+    time: Res<Time>,
+    mut mouse_events: EventReader<MouseMotion>,
+    key_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut CameraController), With<Camera>>,
+) {
+    let mut mouse_delta = Vec2::ZERO;
+    for mouse_event in mouse_events.iter() {
+        mouse_delta += mouse_event.delta;
+    }
+
+    for (mut transform, mut options) in &mut query {
+        if !options.enabled {
+            continue;
+        }
+
+        let mut axis_input = Vec3::ZERO;
+        if key_input.pressed(options.key_up) {
+            axis_input.y += 1.0;
+        }
+        if key_input.pressed(options.key_down) {
+            axis_input.y -= 1.0;
+        }
+
+        if axis_input != Vec3::ZERO {
+            let max_speed = options.walk_speed;
+        };
+
+        if mouse_delta != Vec2::ZERO {}
+    }
+}
