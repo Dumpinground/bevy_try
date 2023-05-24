eda243f14126ff3ae9ac6c3407d4643dd69d9ced
Dumpinground <e60533658@icloud.com>
2023-05-25 00:06:14 +0800

    update

--- examples/bevy-ball-game/main_menu/styles.rs
+++ examples/bevy-ball-game/main_menu/styles.rs
@@ -8,6 +8,22 @@ pub const PRESSED_BUTTON_COLOR: Color = Color::rgb(0.35, 0.75, 0.35);
 // pub const FONT_PATH: &str = "ball-game/fonts/FiraSans-Bold.ttf";
 pub const FONT_PATH: &str = "ball-game/fonts/JOKERMAN.TTF";
 
+pub struct FontSets;
+
+impl FontSets {
+    fn jokerman() -> &'static str {
+        "ball-game/fonts/JOKERMAN.TTF"
+    }
+
+    fn edwardian() -> &'static str {
+        "ball-game/fonts/ITCEDSCR.TTF"
+    }
+
+    fn curlz() -> &'static str {
+        "ball-game/fonts/CURLZ___.TTF"
+    }
+}
+
 pub const MAIN_MENU_STYLE: Style = Style {
     flex_direction: FlexDirection::Column,
     justify_content: JustifyContent::Center,
@@ -40,7 +56,7 @@ pub const BUTTON_STYLE: Style = Style {
 
 pub fn get_title_text_style(assert_server: &Res<AssetServer>) -> TextStyle {
     TextStyle {
-        font: assert_server.load(FONT_PATH),
+        font: assert_server.load(FontSets::jokerman()),
         font_size: 64.0,
         color: Color::WHITE,
     }
@@ -48,7 +64,7 @@ pub fn get_title_text_style(assert_server: &Res<AssetServer>) -> TextStyle {
 
 pub fn get_button_text_style(assert_server: &Res<AssetServer>) -> TextStyle {
     TextStyle {
-        font: assert_server.load(FONT_PATH),
+        font: assert_server.load(FontSets::curlz()),
         font_size: 32.0,
         color: Color::WHITE,
     }
