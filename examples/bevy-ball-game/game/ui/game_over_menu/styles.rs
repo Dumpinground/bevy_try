782381a0f897b33798f3c215e1f2e72af95ea9a1
Ad <DeadLimit@lkr5.onmicrosoft.com>
2023-05-25 14:00:45 +0800

    update

--- examples/bevy-ball-game/game/ui/game_over_menu/styles.rs
+++ examples/bevy-ball-game/game/ui/game_over_menu/styles.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, a11y::accesskit::TextAlign};
+use bevy::{a11y::accesskit::TextAlign, prelude::*};
 
 pub const BACKGROUND_COLOR: Color = Color::rgba(0.25, 0.25, 0.25, 0.5);
 
@@ -15,9 +15,25 @@ pub const BUTTON_STYLE: Style;
 pub const FONT_PATH: &str = "ball/game/fonts/JOKERMAN.TTF";
 
 pub fn get_title_text_style(asset_server: &Res<AssetServer>) -> TextStyle {
-    TextStyle { font: asset_server.load(FONT_PATH), font_size: 64.0, color: Color::rgb(1.0, 1.0, 1.0) }
+    TextStyle {
+        font: asset_server.load(FONT_PATH),
+        font_size: 64.0,
+        color: Color::rgb(1.0, 1.0, 1.0),
+    }
 }
 
 pub fn get_final_score_text_style(asset_server: &Res<AssetServer>) -> TextStyle {
-    TextStyle { font: asset_server.load(FONT_PATH), font_size: 48.0, color: Color::rgb(1.0, 1.0, 1.0) }
+    TextStyle {
+        font: asset_server.load(FONT_PATH),
+        font_size: 48.0,
+        color: Color::rgb(1.0, 1.0, 1.0),
+    }
+}
+
+pub fn get_button_text_style(asset_server: &Res<AssetServer>) -> TextStyle {
+    TextStyle {
+        font: asset_server.load(FONT_PATH),
+        font_size: 32.0,
+        color: Color::rgb(1.0, 1.0, 1.0),
+    }
 }
--- examples/bevy-ball-game/game/ui/game_over_menu/systems/layout.rs
+++ examples/bevy-ball-game/game/ui/game_over_menu/systems/layout.rs
@@ -1,3 +1,5 @@
+use std::panic::get_backtrace_style;
+
 use bevy::prelude::*;
 
 use super::super::components::*;
@@ -9,47 +11,133 @@ pub fn spawn_game_over_menu(mut commands: Commands, asset_server: Res<AssetServe
 
 pub fn build_game_over_menu(commands: &mut Commands, asset_server: &Res<AssetServer>) -> Entity {
     let game_over_menu_entity = commands
-        .spawn((NodeBundle {
-            style: GAME_OVER_MENU_STYLE,
-            z_index: ZIndex::Local(2),
-            ..default(),
-        }, 
-            GameOverMenu {}, 
-        )).with_children(|parent| {
-            parent.spawn(NodeBundle {
-                style: GAME_OVER_MENU_CONTAINER_STYLE,
-                background_color: BACKGROUND_COLOR,
+        .spawn((
+            NodeBundle {
+                style: GAME_OVER_MENU_STYLE,
+                z_index: ZIndex::Local(2),
                 ..default()
-            })
-            .with_children(|parent| {
+            },
+            GameOverMenu {},
+        ))
+        .with_children(|parent| {
+            parent
+                .spawn(NodeBundle {
+                    style: GAME_OVER_MENU_CONTAINER_STYLE,
+                    background_color: BACKGROUND_COLOR,
+                    ..default()
+                })
+                .with_children(|parent| {
                     // Title
                     parent.spawn(TextBundle {
-                        text: Text { sections: vec![TextSection::new("Game Over", get_title_text_style(&asset_server))], alignment: TextAlignment::Center, ..default() }, ..default()
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "Game Over",
+                                get_title_text_style(&asset_server),
+                            )],
+                            alignment: TextAlignment::Center,
+                            ..default()
+                        },
+                        ..default()
                     });
                     // Final Score Text
-                    parent.spawn((TextBundle {
-                        text: Text { sections: vec![TextSection::new("Your final score was:", get_final_score_text_style(&asset_server))], alignment: TextAlignment::Center,..default() }, ..default()
-                    }, FinalScoreText {} ));
+                    parent.spawn((
+                        TextBundle {
+                            text: Text {
+                                sections: vec![TextSection::new(
+                                    "Your final score was:",
+                                    get_final_score_text_style(&asset_server),
+                                )],
+                                alignment: TextAlignment::Center,
+                                ..default()
+                            },
+                            ..default()
+                        },
+                        FinalScoreText {},
+                    ));
                     // Restart Button
-                    parent.spawn((ButtonBundle {
-                        style: BUTTON_STYLE,
-                        background_color: NORMAL_BUTTON.into(),
-                        ..default()
-                    }, RestartButton {}));
+                    parent
+                        .spawn((
+                            ButtonBundle {
+                                style: BUTTON_STYLE,
+                                background_color: NORMAL_BUTTON.into(),
+                                ..default()
+                            },
+                            RestartButton {},
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn(TextBundle {
+                                style: Style::DEFAULT,
+                                text: Text {
+                                    sections: vec![TextSection::new(
+                                        "Restart",
+                                        get_button_text_style(&asset_server),
+                                    )],
+                                    alignment: TextAlignment::Center,
+                                    ..default()
+                                },
+                                ..default()
+                            });
+                        });
                     // Main Menu Button
-                    parent.spawn((ButtonBundle {
-                        style: BUTTON_STYLE,
-                        background_color: NORMAL_BUTTON.into(),
-                        ..default()
-                    }, MainMenuButton {}));
+                    parent
+                        .spawn((
+                            ButtonBundle {
+                                style: BUTTON_STYLE,
+                                background_color: NORMAL_BUTTON.into(),
+                                ..default()
+                            },
+                            MainMenuButton {},
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn(TextBundle {
+                                style: Style::DEFAULT,
+                                text: Text {
+                                    sections: vec![TextSection::new(
+                                        "Main Menu",
+                                        get_button_text_style(&asset_server),
+                                    )],
+                                    alignment: TextAlignment::Center,
+                                    ..default()
+                                },
+                                ..default()
+                            });
+                        });
                     // Quit Button
-                    parent.spawn((ButtonBundle {
-                        style: BUTTON_STYLE,
-                        background_color: NORMAL_BUTTON.into(),
-                        ..default()
-                    }, QuitButton {}));
+                    parent
+                        .spawn((
+                            ButtonBundle {
+                                style: BUTTON_STYLE,
+                                background_color: NORMAL_BUTTON.into(),
+                                ..default()
+                            },
+                            QuitButton {},
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn(TextBundle {
+                                style: Style::DEFAULT,
+                                text: Text {
+                                    sections: vec![TextSection::new(
+                                        "Quit",
+                                        get_button_text_style(&asset_server),
+                                    )],
+                                    alignment: TextAlignment::Center,
+                                    ..default()
+                                },
+                                ..default()
+                            });
+                        });
                 });
-        }).id();
+        })
+        .id();
 
     game_over_menu_entity
+}
+
+pub fn despawn_game_over_menu(
+    mut commands: Commands,
+    game_over_menu_query: Query<Entity, With<GameOverMenu>>,
+) {
+    if let Ok(game_over_menu_entity) = game_over_menu_query.get_single() {
+        commands.entity(game_over_menu_entity).despawn_recursive();
+    }
 }
