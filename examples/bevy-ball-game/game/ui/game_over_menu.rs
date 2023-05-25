f941aaa8bee7827d65b9163b9e195c0aee9cd6a4
Dumpinground <56429201+Dumpinground@users.noreply.github.com>
2023-05-25 09:03:10 +0000

    update

--- examples/bevy-ball-game/game/ui/game_over_menu.rs
+++ examples/bevy-ball-game/game/ui/game_over_menu.rs
@@ -4,10 +4,25 @@ mod systems;
 
 use bevy::prelude::*;
 
+use systems::{
+    layout::*,
+    interactions::*,
+    updates::*,
+};
+
+use crate::AppState;
+
 pub struct GameOverMenuPlugin;
 
 impl Plugin for GameOverMenuPlugin {
     fn build(&self, app: &mut App) {
-        
+        app.add_system(spawn_game_over_menu.in_schedule(OnEnter(AppState::GameOver)))
+        .add_systems((
+            interact_with_restart_button,
+            interact_with_main_menu_button,
+            interact_with_quit_button,
+            update_final_score_text
+        ).in_set(OnUpdate(AppState::GameOver)))
+        .add_system(despawn_game_over_menu.in_schedule(OnExit(AppState::GameOver)));
     }
 }
--- examples/bevy-ball-game/game/ui/hud.rs
+++ examples/bevy-ball-game/game/ui/hud.rs
@@ -1,1 +1,22 @@
+mod components;
+mod styles;
+mod systems;
+
 use bevy::prelude::*;
+
+use crate::AppState;
+
+use systems::{
+    layout::*,
+    updates::*,
+};
+
+pub struct HudPlugin;
+
+impl Plugin for HudPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(spawn_hud.in_schedule(OnEnter(AppState)))
+        .add_systems((update_score_text, update_enemy_text).in_set(OnUpdate(AppState::Game)))
+        .add_system(despawn_hud.in_schedule(OnExit(AppState::Game)));
+    }
+}
