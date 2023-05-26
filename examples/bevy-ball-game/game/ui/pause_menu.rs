9744f2352f529317503c4257357822e42e7fd95c
Dumpinground <e605336584@icloud.com>
2023-05-26 15:17:08 +0800

    update

--- examples/bevy-ball-game/game/ui/pause_menu.rs
+++ examples/bevy-ball-game/game/ui/pause_menu.rs
@@ -1,1 +1,13 @@
+mod components;
+mod styles;
+mod systems;
+
 use bevy::prelude::*;
+
+pub struct PauseMenuPlugin;
+
+impl Plugin for PauseMenuPlugin {
+    fn build(&self, app: &mut App) {
+        
+    }
+}
