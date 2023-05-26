074612415e48465dc6cebbfb78b951ae66976e54
Dumpinground <e605336584@icloud.com>
2023-05-26 14:27:42 +0800

    update

--- examples/bevy-ball-game/game/ui.rs
+++ examples/bevy-ball-game/game/ui.rs
@@ -4,8 +4,13 @@ mod pause_menu;
 
 use bevy::prelude::*;
 
+use self::{hud::HudPlugin, game_over_menu::GameOverMenuPlugin};
+
 pub struct GameUiPlugin;
 
 impl Plugin for GameUiPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_plugin(HudPlugin)
+        .add_plugin(GameOverMenuPlugin)
+    }
 }
