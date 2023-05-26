bc7a191993d25d65b3ecbd47bb407573f796f0b9
Dumpinground <e605336584@icloud.com>
2023-05-26 10:10:35 +0800

    update

--- examples/bevy-ball-game/game/ui/game_over_menu/styles.rs
+++ examples/bevy-ball-game/game/ui/game_over_menu/styles.rs
@@ -6,11 +6,31 @@ pub const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
 pub const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
 pub const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
 
-pub const GAME_OVER_MENU_STYLE: Style;
+pub const GAME_OVER_MENU_STYLE: Style = Style {
+    position_type: PositionType::Absolute,
+    display: Display::Flex,
+    justify_content: JustifyContent::Center,
+    align_items: AlignItems::Center,
+    size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+    ..Style::DEFAULT
+};
 
-pub const GAME_OVER_MENU_CONTAINER_STYLE: Style;
+pub const GAME_OVER_MENU_CONTAINER_STYLE: Style = Style {
+    display: Display::Flex,
+    flex_direction: FlexDirection::Column,
+    justify_content: JustifyContent::Center,
+    align_items: AlignItems::Center,
+    size: Size::new(Val::Px(400.0), Val::Px(400.0)),
+    gap: Size::new(Val::Px(8.0), Val::Px(8.0)),
+    ..Style::DEFAULT
+};
 
-pub const BUTTON_STYLE: Style;
+pub const BUTTON_STYLE: Style = Style {
+    size: Size::new(Val::Px(200.0), Val::Px(200.0)),
+    justify_content: JustifyContent::Center,
+    align_items: AlignItems::Center,
+    ..Style::DEFAULT
+};
 
 pub const FONT_PATH: &str = "ball/game/fonts/JOKERMAN.TTF";
 
