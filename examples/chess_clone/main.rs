3cd0f50a60c61511afb0c92f2662ed073c09b51a
Dumpinground <e605336584@icloud.com>
2023-06-01 14:20:34 +0800

    update

--- examples/chess_clone/main.rs
+++ examples/chess_clone/main.rs
@@ -1,5 +1,34 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).add_resource().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(7.0).into()),
+        material: materials.add(Color::rgb(1., 0.9, 0.9).into()),
+        transform: Transform::from_xyz(4., 0., 4.),
+        ..default()
+    });
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_matrix(Mat4::from_rotation_translation(
+            Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
+            Vec3::new(-7., 20., 4.),
+        )),
+        ..default()
+    });
+
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(4., 8., 4.),
+        ..default()
+    });
 }
