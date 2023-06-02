4a5c8665c3742e3547bf6ca754d331b6e0f63361
Dumpinground <e605336584@icloud.com>
2023-06-02 13:58:09 +0800

    update

--- examples/chess_clone/main.rs
+++ examples/chess_clone/main.rs
@@ -2,8 +2,17 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .insert_resource(Msaa::Sample4)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Chess".into(),
+                resolution: (800., 400.).into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_startup_system(setup)
+        .add_startup_system(create_board)
         .run();
 }
 
@@ -22,7 +31,7 @@ fn setup(
     commands.spawn(Camera3dBundle {
         transform: Transform::from_matrix(Mat4::from_rotation_translation(
             Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
-            Vec3::new(-7., 20., 4.),
+            Vec3::new(-7., 13., 4.),
         )),
         ..default()
     });
@@ -31,4 +40,29 @@ fn setup(
         transform: Transform::from_xyz(4., 8., 4.),
         ..default()
     });
+}
+
+fn create_board(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let mesh = meshes.add(shape::Plane::from_size(1.).into());
+    let white_material = materials.add(Color::rgb(1., 0.9, 0.9).into());
+    let black_material = materials.add(Color::rgb(0., 0.1, 0.1).into());
+
+    for i in 0..8 {
+        for j in 0..8 {
+            commands.spawn(PbrBundle {
+                mesh: mesh.clone(),
+                material: if (i + j + 1) % 2 == 0 {
+                    white_material.clone()
+                } else {
+                    black_material.clone()
+                },
+                transform: Transform::from_xyz(i as f32, 0., j as f32),
+                ..default()
+            });
+        }
+    }
 }
