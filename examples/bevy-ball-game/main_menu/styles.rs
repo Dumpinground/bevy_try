1cef0fe2bf92b144f6cae75a63c9745543816845
Dumpinground <e605336584@icloud.com>
2023-07-11 08:53:53 +0800

    update

--- examples/bevy-ball-game/main_menu/styles.rs
+++ examples/bevy-ball-game/main_menu/styles.rs
@@ -5,40 +5,88 @@ pub const NORMAL_BUTTON_COLOR: Color = Color::rgb(0.15, 0.15, 0.15);
 pub const HOVERED_BUTTON_COLOR: Color = Color::rgb(0.25, 0.25, 0.25);
 pub const PRESSED_BUTTON_COLOR: Color = Color::rgb(0.35, 0.75, 0.35);
 
-pub const MAIN_MENU_STYLE: Style = Style {
-    flex_direction: FlexDirection::Column,
-    justify_content: JustifyContent::Center,
-    align_items: AlignItems::Center,
-    width: Val::Percent(100.0),
-    height: Val::Percent(100.0),
-    row_gap: Val::Px(8.0),
-    column_gap: Val::Px(8.0),
-    ..Style::DEFAULT
-};
+struct Styles;
 
-pub const TITLE_STYLE: Style = Style {
-    flex_direction: FlexDirection::Row,
-    justify_content: JustifyContent::Center,
-    align_items: AlignItems::Center,
-    width: Val::Px(300.0),
-    height: Val::Px(120.0),
-    ..Style::DEFAULT
-};
+impl Styles {
+    fn main_menu() -> Style {
+        Style {
+            flex_direction: FlexDirection::Column,
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            row_gap: Val::Px(8.0),
+            column_gap: Val::Px(8.0),
+            ..Style::DEFAULT
+        }
+    }
 
-pub const IMAGE_STYLE: Style = Style {
-    width: Val::Px(64.0),
-    height: Val::Px(64.0),
-    margin: UiRect::new(Val::Px(8.0), Val::Px(8.0), Val::Px(8.0), Val::Px(8.0)),
-    ..Style::DEFAULT
-};
+    fn title_style() -> Style {
+        Style {
+            flex_direction: FlexDirection::Row,
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            width: Val::Px(300.0),
+            height: Val::Px(120.0),
+            ..Style::DEFAULT
+        }
+    }
 
-pub const BUTTON_STYLE: Style = Style {
-    justify_content: JustifyContent::Center,
-    align_items: AlignItems::Center,
-    width: Val::Px(200.0),
-    height: Val::Px(80.0),
-    ..Style::DEFAULT
-};
+    fn image_style() -> Style {
+        Style {
+            width: Val::Px(64.0),
+            height: Val::Px(64.0),
+            margin: UiRect::new(Val::Px(8.0), Val::Px(8.0), Val::Px(8.0), Val::Px(8.0)),
+            ..Style::DEFAULT
+        }
+    }
+
+    fn button_style() -> Style {
+        Style {
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            width: Val::Px(200.0),
+            height: Val::Px(80.0),
+            ..Style::DEFAULT
+        }
+    }
+}
+
+// Bevy 0.11.0 cannot use ..Style::DEFAULT in constants
+// pub const MAIN_MENU_STYLE: Style = Style {
+//     flex_direction: FlexDirection::Column,
+//     justify_content: JustifyContent::Center,
+//     align_items: AlignItems::Center,
+//     width: Val::Percent(100.0),
+//     height: Val::Percent(100.0),
+//     row_gap: Val::Px(8.0),
+//     column_gap: Val::Px(8.0),
+//     ..Style::DEFAULT
+// };
+
+// pub const TITLE_STYLE: Style = Style {
+//     flex_direction: FlexDirection::Row,
+//     justify_content: JustifyContent::Center,
+//     align_items: AlignItems::Center,
+//     width: Val::Px(300.0),
+//     height: Val::Px(120.0),
+//     ..Style::DEFAULT
+// };
+
+// pub const IMAGE_STYLE: Style = Style {
+//     width: Val::Px(64.0),
+//     height: Val::Px(64.0),
+//     margin: UiRect::new(Val::Px(8.0), Val::Px(8.0), Val::Px(8.0), Val::Px(8.0)),
+//     ..Style::DEFAULT
+// };
+
+// pub const BUTTON_STYLE: Style = Style {
+//     justify_content: JustifyContent::Center,
+//     align_items: AlignItems::Center,
+//     width: Val::Px(200.0),
+//     height: Val::Px(80.0),
+//     ..Style::DEFAULT
+// };
 
 pub fn get_title_text_style(assert_server: &Res<AssetServer>) -> TextStyle {
     TextStyle {
