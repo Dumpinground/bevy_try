7bbc0e6dd8937f2be1bd23b6553a03867b1f4151
Dumpinground <e605336584@icloud.com>
2023-09-25 18:14:24 +0800

    update

---
+++ examples/3rd_tutorial/main.rs
@@ -0,0 +1,22 @@
+mod camera;
+mod player;
+mod world;
+
+use camera::CameraPlugin;
+use player::PlayerPlugin;
+use world::WorldPlugin;
+
+use bevy::prelude::*;
+use bevy_third_person_camera::*;
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            PlayerPlugin,
+            CameraPlugin,
+            WorldPlugin,
+            ThirdPersonCameraPlugin,
+        ))
+        .run();
+}
