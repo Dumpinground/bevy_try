e664dd8e1e1b4d228073cd6a1fbae6806f8becfb
Dumpinground <e605336584@icloud.com>
2024-12-08 15:52:09 +0800

    bump to bevy-0.15

--- examples/custom_scene.rs
+++ examples/custom_scene.rs
@@ -15,63 +15,41 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     // plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(7.0).into()),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(7., 7.))),
+        MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
+    ));
 
-    // cubes
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(2.0, 0.5, 0.0),
-        ..default()
-    });
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(-2.0, 0.5, 0.0),
-        ..default()
-    });
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 2.0),
-        ..default()
-    });
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, -2.0),
-        ..default()
-    });
+    // Cubes
+    let cube_mesh = Mesh3d(meshes.add(Cuboid::default()));
+    let cube_material = MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6)));
+
+    let translations = [
+        Vec3::new(0.0, 0.5, 0.0),
+        Vec3::new(2.0, 0.5, 0.0),
+        Vec3::new(-2.0, 0.5, 0.0),
+        Vec3::new(0.0, 0.5, 2.0),
+        Vec3::new(0.0, 0.5, -2.0),
+    ];
+
+    for t in translations {
+        commands.spawn((
+            cube_mesh.clone(),
+            cube_material.clone(),
+            Transform::from_translation(t),
+        ));
+    }
 
     // light
     commands.spawn((
-        PointLightBundle {
-            point_light: PointLight {
-                intensity: 1500.0,
-                shadows_enabled: true,
-                ..default()
-            },
-            transform: Transform::from_xyz(4.0, 8.0, 4.0),
-            ..default()
-        },
+        PointLight::default(),
+        Transform::from_xyz(4.0, 8.0, 4.0),
         Light::default(),
     ));
     // camera
     commands.spawn((
-        Camera3dBundle {
-            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..default()
-        },
+        Camera3d::default(),
+        Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         CameraController::default(),
     ));
 }
@@ -103,8 +81,8 @@ impl Default for CameraController {
         Self {
             enabled: true,
 
-            key_up: KeyCode::Up,
-            key_down: KeyCode::Down,
+            key_up: KeyCode::ArrowUp,
+            key_down: KeyCode::ArrowDown,
 
             walk_speed: 3.0,
             run_speed: 5.0,
@@ -122,18 +100,18 @@ fn light_moving(time: Res<Time>, mut light_query: Query<(&mut Transform, &mut Li
 
         direction += Vec3::new(1.0, 0.0, 0.0) * light.speed;
 
-        transform.translation += direction * time.delta_seconds();
+        transform.translation += direction * time.delta_secs();
     }
 }
 
 fn camera_controller(
     time: Res<Time>,
     mut mouse_events: EventReader<MouseMotion>,
-    key_input: Res<Input<KeyCode>>,
+    key_input: Res<ButtonInput<KeyCode>>,
     mut query: Query<(&mut Transform, &mut CameraController), With<Camera>>,
 ) {
     let mut mouse_delta = Vec2::ZERO;
-    for mouse_event in mouse_events.iter() {
+    for mouse_event in mouse_events.read() {
         mouse_delta += mouse_event.delta;
     }
 
@@ -159,34 +137,34 @@ fn camera_controller(
 }
 
 fn key_event(
-    keyboard_input: Res<Input<KeyCode>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
     mut camera_query: Query<(&mut Transform, &mut CameraController)>,
     time: Res<Time>,
 ) {
     if let Ok((mut transform, camera)) = camera_query.get_single_mut() {
         let mut direction = Vec3::ZERO;
 
-        if keyboard_input.pressed(KeyCode::Q) {
+        if keyboard_input.pressed(KeyCode::KeyQ) {
             direction += Vec3::new(0.0, 1.0, 0.0);
         }
 
-        if keyboard_input.pressed(KeyCode::A) {
+        if keyboard_input.pressed(KeyCode::KeyA) {
             direction += Vec3::new(0.0, -1.0, 0.0);
         }
 
-        if keyboard_input.pressed(KeyCode::S) {
+        if keyboard_input.pressed(KeyCode::KeyS) {
             direction += Vec3::new(-1.0, 0.0, 0.0);
         }
 
-        if keyboard_input.pressed(KeyCode::E) {
+        if keyboard_input.pressed(KeyCode::KeyE) {
             direction += Vec3::new(1.0, 0.0, 0.0);
         }
 
-        if keyboard_input.pressed(KeyCode::W) {
+        if keyboard_input.pressed(KeyCode::KeyW) {
             direction += Vec3::new(0.0, 0.0, -1.0);
         }
 
-        if keyboard_input.pressed(KeyCode::D) {
+        if keyboard_input.pressed(KeyCode::KeyD) {
             direction += Vec3::new(0.0, 0.0, 1.0);
         }
 
@@ -194,6 +172,6 @@ fn key_event(
             direction = direction.normalize();
         }
 
-        transform.translation += direction * camera.walk_speed * time.delta_seconds();
+        transform.translation += direction * camera.walk_speed * time.delta_secs();
     }
 }
